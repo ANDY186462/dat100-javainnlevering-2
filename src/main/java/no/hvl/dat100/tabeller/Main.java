201e972621313b88de32eb54d50bd7b06a31625e
Alexrohjak <alexander.rohde.jakobsen@gmail.com>
2024-10-01 13:24:34 +0200

    D+FERDIG

--- src/main/java/no/hvl/dat100/tabeller/Main.java
+++ src/main/java/no/hvl/dat100/tabeller/Main.java
@@ -9,73 +9,51 @@ public class Main {
 		int tall = 5;
 
 		if (Tabeller.finnesTall(tabell, tall)) {
-
 			System.out.println(tall + " finnes i tabellen.");
-
 		} else {
-
 			System.out.println(tall + " finnes ikke i tabellen.");
-
 		}
 
 		int posisjon = Tabeller.posisjonTall(tabell, tall);
 
 		if (posisjon != -1) {
-
 			System.out.println("Tallet " + tall + " finnes på posisjon: " + posisjon);
-
 		} else {
-
 			System.out.println("Tallet " + tall + " finnes ikke i tabellen.");
 		}
 
 		int[] reversTabell = Tabeller.reverser(tabell);
-
-		System.out.println("Reversert tabell: ");
-
 		System.out.print("Reversert tabell: ");
-
 		for (int r : reversTabell) {
-
 			System.out.print(r + " ");
-
 		}
-
 		System.out.println();
 
-		System.out.println();
-
-		boolean Sortert = Tabeller.erSortert(tabell);
-
-		if (Sortert) {
-
-			System.out.println(tabell + "er sortert riktig");
-
+		boolean sortert = Tabeller.erSortert(tabell);
+		if (sortert) {
+			System.out.println("Tabellen er sortert riktig.");
 		} else {
-
-			System.out.println(tabell + "er sortert feil");
-
+			System.out.println("Tabellen er sortert feil.");
 		}
 
 		int[] tabell1 = { 1, 2, 3 };
-
 		int[] tabell2 = { 4, 5, 6 };
-
 		int[] resultat = Tabeller.settSammen(tabell1, tabell2);
 
+		System.out.print("Sammensatt tabell: ");
 		for (int t : resultat) {
-
-			System.out.println(t + " ");
-
-			int[] tabell1 = { 8, 9, 10 };
-			int[] tabell2 = { 11, 12, 13 };
-			int[] resultat = Tabeller.settSammen(tabell1, tabell2);
-
-			for (int t : resultat) {
-				System.out.print(t + " ");
-
-			}
-
+			System.out.print(t + " ");
 		}
+		System.out.println();
+
+		int[] tabell3 = { 8, 9, 10 };
+		int[] tabell4 = { 11, 12, 13 };
+		int[] resultat2 = Tabeller.settSammen(tabell3, tabell4);
+
+		System.out.print("Sammensatt tabell 2: ");
+		for (int t : resultat2) {
+			System.out.print(t + " ");
+		}
+		System.out.println();
 	}
 }
--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -103,22 +103,16 @@ public class Tabeller {
 	// g)
 	public static boolean erSortert(int[] tabell) {
 
-		for (int i = 1; i < tabell.length; i++) {
-			if (tabell[i] < tabell[i - 1]) { // Endret til '<' for å sjekke sortering i stigende rekkefølge
+		for (int i = 0; i < tabell.length - 1; i++) {
 
-				for (int i1 = 0; i1 < tabell.length - 1; i1++) {
-					if (tabell[i1] > tabell[i1 + 1]) {
+			if (tabell[i] > tabell[i + 1]) {
 
-						return false;
-					}
-				}
-
-				return true;
+				return false;
 
 			}
 		}
-		return false;
 
+		return true;
 	}
 
 	// h)
@@ -129,7 +123,6 @@ public class Tabeller {
 		for (int i = 0; i < tabell1.length; i++) {
 
 			tabell3[i] = tabell1[i];
-
 		}
 
 		for (int j = 0; j < tabell2.length; j++) {
@@ -137,10 +130,6 @@ public class Tabeller {
 			tabell3[tabell1.length + j] = tabell2[j];
 		}
 
-		for (int j = 0; j < tabell2.length; j++) {
-			tabell3[j] = tabell2[j];
-
-		}
 		return tabell3;
 	}
 }
